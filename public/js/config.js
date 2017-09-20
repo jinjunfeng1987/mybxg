efe4350ea9536ac87ad16b28e516530358ec3499
jin <409511704@qq.com>
2017-09-20 15:01:37 +0800

    模板引擎重构登录功能

--- public/js/config.js
+++ public/js/config.js
@@ -2,10 +2,11 @@
  * Created by ZHANG on 2017/9/19.
  */
 require.config({
-	baseUrl : '/public/assets/',
+	baseUrl : '/public/assets',
 	paths : {
 		jquery : 'jquery/jquery',
 		cookie : 'jquery-cookie/jquery.cookie',
+		template : 'artTemplate/template-web',
 		common : '../js/common',
 		login : '../js/login'
 	}
--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ define(['jquery','cookie'], function ($) {
 			success : function (data) {
 				if(data.code == 200){
 					//把用户的登录信息存储到cookie中，方便跨页面共享数据
-					$.cookie('loginInfo',JSON.stringify(data.result),{path : '/'});
+					$.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
 					//登录成功，跳转到主页面
 					location.href = '/main/index';
 				}
