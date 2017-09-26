66e43230ec08b154d2ffb92b248fbd558307a90e
jin <409511704@qq.com>
2017-09-26 09:14:46 +0800

    添加富文本功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,33 +1,38 @@
 /**
  * Created by ZHANG on 2017/9/25.
  */
-define(['jquery','template','uploadify','region'], function ($,template) {
+define(['jquery', 'template', 'ckeditor', 'uploadify', 'region','datepicker','language'], function ($, template, CKEDITOR) {
 	$.ajax({
-		type : 'get',
-		url : '/api/teacher/profile',
-		dataType : 'json',
-		success : function (data) {
-
-			var html = template('settingsTpl',data.result);
+		type: 'get',
+		url: '/api/teacher/profile',
+		dataType: 'json',
+		success: function (data) {
+			var html = template('settingsTpl', data.result);
 			$('#settingsInfo').html(html);
 			//处理头像上传
 			$('#upfile').uploadify({
-				width :120,
-				height :120,
-				buttonText:'',
-				itemTemplate : '<span></span>',
-				swf :'/public/assets/uploadify/uploadify.swf',
-				uploader : '/api/uploader/avatar',
-				fileObjName : 'tc_avatar',
-				onUploadSuccess : function (a,b) {
+				width: 120,
+				height: 120,
+				buttonText: '',
+				itemTemplate: '<span></span>',
+				swf: '/public/assets/uploadify/uploadify.swf',
+				uploader: '/api/uploader/avatar',
+				fileObjName: 'tc_avatar',
+				onUploadSuccess: function (a, b) {
 					var obj = JSON.parse(b);
-					$('.preview img').attr('src',obj.result.path);
+					$('.preview img').attr('src', obj.result.path);
 				}
 			});
 			//处理省市县三级联动
 			$('#pcd').region({
-				url : '/public/assets/jquery-region/region.json',
-
+				url: '/public/assets/jquery-region/region.json',
+			});
+			//处理富文本
+			CKEDITOR.replace('editor', {
+				toolbarGroups: [
+					{name: 'clipboard', groups: ['clipboard', 'undo']},
+					{name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']}
+				]
 			});
 		}
 	});
